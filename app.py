d2ffec4c06e70e91e58a95f207ff34c78162aaaa
AbilPratama <146618837+AbilPratama@users.noreply.github.com>
2025-06-19 22:45:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import streamlit as st
 import joblib
-import os
 
 # Load model dan vectorizer
 model = joblib.load("model_random_forest.pkl")
 vectorizer = joblib.load("tfidf_vectorizer.pkl")
 
-# UI aplikasi Streamlit
-st.set_page_config(page_title="Klasifikasi Komentar Webtoon", layout="centered")
-st.title("📚 Klasifikasi Sentimen Komentar Webtoon")
-st.write("Masukkan komentar dari pengguna Webtoon untuk diklasifikasikan sebagai **positif** atau **negatif**.")
+# UI Streamlit
+st.set_page_config(page_title="Klasifikasi Komentar Youtube", layout="centered")
+st.title("📚 Klasifikasi Sentimen Komentar Youtube")
+st.write("Masukkan komentar dari pengguna")
 
-# Input dari pengguna
+# Input teks pengguna
 user_input = st.text_area("Komentar pengguna:", height=150)
 
 # Tombol klasifikasi
@@ -19,12 +18,9 @@ if st.button("Klasifikasikan"):
     if not user_input.strip():
         st.warning("⚠️ Komentar tidak boleh kosong.")
     else:
-        # Cek berdasarkan keyword terlebih dahulu
-        keyword_result = keyword_sentiment(user_input)
-        if keyword_result:
-            st.success(f"Hasil klasifikasi: {keyword_result}")
-        else:
-            # Klasifikasi menggunakan model machine learning
-            input_vector = vectorizer.transform([user_input])
-            prediction = model.predict(input_vector)[0]
-            st.success(f"Hasil klasifikasi model: {prediction}")
+        # Transform dan prediksi
+        input_vector = vectorizer.transform([user_input])
+        prediction = model.predict(input_vector)[0]
+
+        # Tampilkan hasil klasifikasi
+        st.success(f"💬 Hasil klasifikasi: **{prediction.upper()}**")
